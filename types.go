f7102eb3677c7f1aa3853d94eee2f7a143ede75b
Buratud Thumboon <buratud@gmail.com>
2025-03-29 16:53:39 +0700

    First commit

---
+++ types.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+
+type SensorData struct {
+	ID        int64     `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	AccelX    float64   `json:"accel_x"`
+	AccelY    float64   `json:"accel_y"`
+	AccelZ    float64   `json:"accel_z"`
+	Latitude  *float64  `json:"latitude"`
+	Longitude *float64  `json:"longitude"`
+}
+
+type SensorDataRequest struct {
+	Size   int       `json:"size" form:"size"`
+	Order  string    `json:"order" form:"order"`
+	Offset *time.Time `json:"offset" form:"offset"`
+}
